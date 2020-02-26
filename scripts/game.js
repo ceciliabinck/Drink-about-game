33cf21584a2e86a99f9cadd5cb17777d4d0b0fb4
Ubuntu <ubuntu@ip-172-31-39-211.ec2.internal>
2020-02-26 15:39:09 +0000

    Inital commit

---
+++ scripts/game.js
@@ -0,0 +1,24 @@
+whatCanIDrink = function(age){
+
+}
+if ("age", function(){ 
+            age < 0;
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+        });
+
+if "age" < 14;
+        return "Drink Toddy";
+        
+        if "age" < 18;
+        return "Drink Coke";
+        
+        if "age" < 21;
+        return "Drink Beer";
+        
+        if "age" < 130;
+        return "Drink Whisky";
+        
+        else {
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    
+    }; */
---
+++ spec/gameSpec.js
@@ -0,0 +1,14 @@
+describe("whatCanIDrink", function(){
+        describe("age", function() {
+            if ("age is less then 0);
+                return ""Sorry. I can’t tell what drink because that age is incorrect!""
+            });
+            
+            describe("age", function() {
+            if ("age is less then 14);
+                return "Drink Toddy";
+            });
+        
+});
+
+/* 
