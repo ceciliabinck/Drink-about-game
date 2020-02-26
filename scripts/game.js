6421d034fcdb23cb032319f007a7cb09543ccd3c
Ubuntu <ubuntu@ip-172-31-39-211.ec2.internal>
2020-02-26 17:29:29 +0000

    Put in the result

--- scripts/game.js
+++ scripts/game.js
@@ -1,24 +1,30 @@
 whatCanIDrink = function(age){
+    
 
-}
-if ("age", function(){ 
-            age < 0;
+    if (age < 0) {
         return "Sorry. I can’t tell what drink because that age is incorrect!";
-        });
+    }
 
-if "age" < 14;
+    else if (age < 14){
         return "Drink Toddy";
+    }
         
-        if "age" < 18;
+    else if (age < 18 && age >= 14) {
         return "Drink Coke";
+    }
         
-        if "age" < 21;
+    else if (age < 21 && age >= 18) {
         return "Drink Beer";
+    }
         
-        if "age" < 130;
+    else if (age < 130) {
         return "Drink Whisky";
+    }
         
-        else {
+    else {
         return "Sorry. I can’t tell what drink because that age is incorrect!";
     
-    }; */
+    }
+    
+};
+
--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,14 +1,48 @@
 describe("whatCanIDrink", function(){
-        describe("age", function() {
-            if ("age is less then 0);
-                return ""Sorry. I can’t tell what drink because that age is incorrect!""
-            });
-            
-            describe("age", function() {
-            if ("age is less then 14);
-                return "Drink Toddy";
-            });
+    
+    beforeEach(function() {
+        drink = new whatCanIDrink();
+    });
+    
+    describe("Checks age", function() {
+        it("should exist", function() {
+            expect(whatCanIDrink).toBeDefined();
+        });
         
-});
-
-/* 
+        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(13)
+            expect(result).toBe("Drink Toddy");
+        });
+        
+        it("should return drink coke when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(17)
+            expect(result).toBe("Drink Coke");
+        });
+        
+        it("should return drink beer when called as whatCanIDrink(20)", function() {
+            var result = whatCanIDrink(20)
+            expect(result).toBe("Drink Beer");
+        });
+        
+        it("should return drink whisky when called as whatCanIDrink(129)", function() {
+            var result = whatCanIDrink(129)
+            expect(result).toBe("Drink Whisky");
+        });
+        
+        it ("should be unable to return a drink when called as whatCanIDrink(130)", function() {
+            var result = whatCanIDrink(130)
+            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        
+        it("should contain Drink when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(10).indexOf("Drink");
+            expect(result).not.toEqual(-1);
+        });
+    
+    });
+    
+});    
+    
+    
+    
+        
